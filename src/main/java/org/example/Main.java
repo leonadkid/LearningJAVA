d719a792bfecac81573d75bc0e08ab8ff648b707
test <leonadkid@gmail.com>
2023-03-02 15:50:20 +0700

    update constractor in main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,18 @@ package org.example;
 
 public class Main
 {
+    private Main()
+    {
+    }
+
     public static void main(final String[] args)
     {
+        boolean check = true;
+        final String text = "abc ";
+        for (int counter = 0; counter < text.length(); counter++) {
+            final boolean currentCheck = Character.isLetter(text.charAt(counter));
+            check = !currentCheck ? false : true;
+        }
+        System.out.println("xxxxxxxx: " + check);
     }
 }
